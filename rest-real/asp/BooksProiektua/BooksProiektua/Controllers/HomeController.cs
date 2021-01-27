0599b4afbd82cc2b2784e5778998e957341b0837
patxibr <71440400+patxibr@users.noreply.github.com>
2021-01-27 12:28:51 +0100

    Update HomeController.cs

--- rest-real/asp/BooksProiektua/BooksProiektua/Controllers/HomeController.cs
+++ rest-real/asp/BooksProiektua/BooksProiektua/Controllers/HomeController.cs
@@ -54,6 +54,7 @@ namespace BooksProiektua.Controllers
                 {
                     haundiegia = true;
                 }
+                
                 //returning the employee list to view  
                 return View(BookList);
             }
